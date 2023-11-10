3478182e6edf1d2222b17b98e3b1a580716954c2
T. Korn <email@tobiaskorn.de>
2023-11-10 14:02:49 +0100

    prepared .NET Solution

---
+++ dotnet/src/PortfolioDescription.net461/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UShell.PortfolioDescription")]
+[assembly: AssemblyDescription("UShell.PortfolioDescription")]
+[assembly: AssemblyProduct("UShell.PortfolioDescription")]
+
+[assembly: AssemblyCompany("(public domain)")]
+[assembly: AssemblyCopyright("SmartStandards")]
+[assembly: AssemblyTrademark("SmartStandards")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("46adc8b8-2f14-4a31-87ca-95b71ee09ea9")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0-localbuild")]
+//DONT ADD: [assembly: AssemblyFileVersion] !!!
---
+++ dotnet/src/PortfolioDescription/PortfolioDescription.cs
@@ -0,0 +1,79 @@
+﻿using Security.AccessTokenHandling;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UShell {
+
+  public class PortfolioDescription {
+
+    /// <summary>
+    /// Application Title
+    /// </summary>
+    public string applicationTitle { get; set; } = "Web-Frontend";
+
+    /// <summary>
+    /// the workspace, where the ushell will tread as landing location
+    /// </summary>
+    public string landingWorkspaceName { get; set; } = "dashboard";
+
+    /// <summary>
+    /// '00000000-0000-0000-0000-000000000000' means DISABLED - NO LOGIN REQUIRED!!!
+    /// </summary>
+    public string primaryUiTokenSourceUid { get; set; } = "00000000-0000-0000-0000-000000000000";
+
+    /// <summary>
+    /// OAuth configuration structure
+    /// </summary>
+    public Dictionary<string, AuthTokenConfig> authTokenConfigs { get; set; } = null;
+
+    /// <summary>
+    /// The fixpoint when resolving a relative URL provided for this value is the
+    /// (portfolio.json)-url where the current PortfolioDescription was loaded from.
+    /// </summary>
+    public string CiDescriptionUrl { get; set; } = null;
+
+    /// <summary>
+    /// The fixpoint when resolving a relative URL provided for this value is the
+    /// (portfolio.json)-url where the current PortfolioDescription was loaded from.
+    /// </summary>
+    public string LegalContactMdUrl  { get; set; } = null;
+
+    /// <summary>
+    /// The fixpoint when resolving a relative URL provided for this value is the
+    /// (portfolio.json)-url where the current PortfolioDescription was loaded from.
+    /// </summary>
+    public string UserAgreementMdUrl  { get; set; } = null;
+
+    /// <summary>
+    /// The Version string is used when persisting the agreement state.
+    /// If it is not equal to the persisted agreement state, then the UI
+    /// will force the user to re-agree!
+    /// </summary>
+    public string UserAgreementVersion { get; set; } = null;
+
+    /// <summary>
+    /// can be used like this: ``` { * "tenant": 4711, "region": "DE" * } ```
+    /// </summary>
+    public Dictionary<string, object> ApplicationScope { get; set; } = null;
+
+    /// <summary>
+    /// inital runtimtags -> !Tag ist also possible
+    /// </summary>
+    public string[] IntialRuntimeTags { get; set; } = null;
+
+    /// <summary>
+    /// Can be used to couple runtimeTags with entries the of the 'scope' claim from the *primaryUiToken*.
+    /// For example: ``` {"showPowerFeatures": "LIC:PremiumSubscription"}```
+    /// </summary>
+    public Dictionary<string, string> RuntimeTagToTokenBinding { get; set; } = null;
+
+    /// <summary>
+    /// The fixpoint when resolving relative URLs provided for this array is the
+    /// (portfolio.json)-url where the current PortfolioDescription was loaded from.
+    /// </summary>
+    public string[] ModuleDescriptionUrls { get; set; } = new string[] { };
+
+  }
+
+}
